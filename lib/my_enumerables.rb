5ac3fff58282d8175efce0b4a55f443349e16e99
misafxd <pisichu.mafs@gmail.com>
2023-12-04 15:58:17 -0600

    Add inject custom function

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -83,6 +83,16 @@ module Enumerable
     end
     new_array
   end
+
+  def my_inject(initial_value)
+    index = 0
+    value = initial_value
+    while index < size
+      value = yield(value, self[index]) if block_given?
+      index += 1
+    end
+    value
+  end
 end
 
 # You will first have to define my_each
@@ -90,6 +100,5 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
 
 end
