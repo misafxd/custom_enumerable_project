9e3db943fbc504999f011b978663649ec5ad1e36
misafxd <pisichu.mafs@gmail.com>
2023-12-04 12:51:32 -0600

    Add my_each custom function

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,17 @@
 module Enumerable
   # Your code goes here
+  def my_each
+    array_size = self.size
+    index = 0
+    new_array = []
+    while index < array_size
+      if block_given?
+        new_array.push(yield(self[index]))
+      end
+      index += 1
+    end
+    self
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +20,5 @@ end
 # to this method
 class Array
   # Define my_each here
+
 end
